2155117f1eef9c246ab15f859d85ce727b0bdb0a
Justin R. Cutler <justin.r.cutler@gmail.com>
2012-03-23 11:42:18 -0400

    Made comments for PyFCDType consistent.

--- pyfcd.c
+++ pyfcd.c
@@ -55,26 +55,26 @@ PyFCD_init(PyFCD *self, PyObject *args, PyObject *kwds)
 
 static PyTypeObject PyFCDType = {
 	PyObject_HEAD_INIT(NULL)
-	0,                         /*ob_size*/
-	"pyfcd.FCD",               /*tp_name*/
-	sizeof(PyFCD),             /*tp_basicsize*/
-	0,                         /*tp_itemsize*/
-	(destructor)PyFCD_dealloc, /*tp_dealloc*/
-	0,                         /*tp_print*/
-	0,                         /*tp_getattr*/
-	0,                         /*tp_setattr*/
-	0,                         /*tp_compare*/
-	0,                         /*tp_repr*/
-	0,                         /*tp_as_number*/
-	0,                         /*tp_as_sequence*/
-	0,                         /*tp_as_mapping*/
-	0,                         /*tp_hash */
-	0,                         /*tp_call*/
-	0,                         /*tp_str*/
-	0,                         /*tp_getattro*/
-	0,                         /*tp_setattro*/
-	0,                         /*tp_as_buffer*/
-	Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+	0,                         /* ob_size */
+	"pyfcd.FCD",               /* tp_name */
+	sizeof(PyFCD),             /* tp_basicsize */
+	0,                         /* tp_itemsize */
+	(destructor)PyFCD_dealloc, /* tp_dealloc */
+	0,                         /* tp_print */
+	0,                         /* tp_getattr */
+	0,                         /* tp_setattr */
+	0,                         /* tp_compare */
+	0,                         /* tp_repr */
+	0,                         /* tp_as_number */
+	0,                         /* tp_as_sequence */
+	0,                         /* tp_as_mapping */
+	0,                         /* tp_hash */
+	0,                         /* tp_call */
+	0,                         /* tp_str */
+	0,                         /* tp_getattro */
+	0,                         /* tp_setattro */
+	0,                         /* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,        /* tp_flags */
 	"FUNcube dongle object",   /* tp_doc */
 	0,                         /* tp_traverse */
 	0,                         /* tp_clear */
